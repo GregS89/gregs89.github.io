7646e62dd56e4ed607c650f9b07917c5fd8f6abc
Greg <gregsimba89@gmail.com>
2023-08-14 10:29:48 +0300

    LeviaKArc

--- js/main.js
+++ js/main.js
@@ -58,39 +58,25 @@
 
     // Header carousel
     $(".header-carousel").owlCarousel({
-        autoplay: false,
-        animateOut: 'fadeOutLeft',
-        items: 1,
-        dots: true,
+        autoplay: true,
+        smartSpeed: 1500,
         loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
+        nav: false,
+        dots: true,
+        items: 1,
+        dotsData: true,
     });
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: false,
+    $('.testimonial-carousel').owlCarousel({
+        autoplay: true,
         smartSpeed: 1000,
-        center: true,
-        dots: false,
         loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
+        nav: false,
+        dots: true,
+        items: 1,
+        dotsData: true,
     });
 
     
